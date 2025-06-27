1605794174155a1d703fccd5bafcd996ad955b5c
Mwirigi <mwirigimburugu81@gmail.com>
2025-06-27 09:21:12 +0300

    superuser

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (
+    User, Agency, Project, ProjectHead, Branch, Outlet, UserOutlet, UAdmin, UAdminAgency, AdminAuthToken, BaAuthToken,
+    AirtelCombined, CokeCombined, BaimsCombined, KspcaCombined, SaffCombined, RedbullOutlet, TotalKenya, AppData, Ba, Backend, BaProject, ProjectAssoc, Containers, ContainerOptions, Coop, Coop2, FormSection, FormSubSection, InputGroup, InputOptions, AuthToken, FormSubmission
+)
 
-# Register your models here.
+models_to_register = [
+    User, Agency, Project, ProjectHead, Branch, Outlet, UserOutlet, UAdmin, UAdminAgency, AdminAuthToken, BaAuthToken,
+    AirtelCombined, CokeCombined, BaimsCombined, KspcaCombined, SaffCombined, RedbullOutlet, TotalKenya, AppData, Ba, Backend, BaProject, ProjectAssoc, Containers, ContainerOptions, Coop, Coop2, FormSection, FormSubSection, InputGroup, InputOptions, AuthToken, FormSubmission
+]
+
+for model in models_to_register:
+    admin.site.register(model)
